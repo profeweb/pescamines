d10fbf0bb8c4095c86fb8c0f309504ec270bfb08
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2024-06-27 10:31:57 +0200

    initial

---
+++ src/step01/Pescamines.java
@@ -0,0 +1,22 @@
+package step01;
+
+import processing.core.PApplet;
+
+public class Pescamines extends PApplet {
+
+    public void settings(){
+        size(800, 800);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("step01.Pescamines");
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+        background(255);
+    }
+}
